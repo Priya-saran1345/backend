e52209c041b9b3f1227aa8dfe0cedca66e4aea7e
Priya <priya.saran.w3era@gmail.com>
2025-02-19 10:11:32 +0530

    bcrypt function

--- src/projectmodel/user.model.js
+++ src/projectmodel/user.model.js
@@ -1,21 +1,64 @@
-import  mongoose from 'mongoose';
+import mongoose from 'mongoose';
+import jwt from 'jwt';
+import bcrypt from 'bcrypt';
 
-const UserSchema=mongoose.Schema({
-
-    username:{
+const UserSchema = mongoose.Schema({
+    username: {
         type: 'string',
         required: true,
         unique: true,
         lowercase: true,
-        trim:true,
-        index:true
+        trim: true,
+        index: true
     },
-    email:{
+    email: {
         type: 'string',
         required: true,
-
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName
+        : {
+        type: 'string',
+        required: true,
+        trim: true,
+        index: true
+    },
+    avtar: {
+        type: 'string',//url of cloudinary
+        required: true,
+    },
+    coverImage: {
+        type: 'string',//url of cloudinary
+    },
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Video'
+        }
+    ],
+    password: {
+        type: 'string',
+        required: [true, 'password is required'],
+        minlength: 8,
+        select: false // to not expose password in response
+    },
+    refreshToken: {
+        type: 'string',
     }
+}, {
+    timestamps: true,
+
+});
+
+UserSchema.pre("save", async function (req, res,next) {
+    if(!this.modified("password"))return next();
+this.password=bcrypt.hash(this.password , 10);
+next()
 })
+UserSchema.methods.isPasswordCorrect=async function (password){
+ return await bcrypt.compare(password ,this.password)
+}
 
-
- export const User=mongoose.model('User',UserSchema);
+export const User = mongoose.model('User', UserSchema);
